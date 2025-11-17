8b0d3ba2ac4eaa6a1aa528e514ee13b48c516af7
XislatRaximjonov <yapon11xislat@gmail.com>
2025-11-17 12:53:36 +0500

    hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,27 @@
+a = float(input("Square side: "))
+perimeter = 4 * a
+area = a * a
+
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+import math
+
+d = float(input("Diameter: "))
+length = math.pi * d
+
+print("Length:", length)
+
+a = float(input("a = "))
+b = float(input("b = "))
+
+mean = (a + b) / 2
+print("Mean:", mean)
+
+a = float(input("a = "))
+b = float(input("b = "))
+
+print("Sum:", a + b)
+print("Product:", a * b)
+print("Square of a:", a**2)
+print("Square of b:", b**2)
